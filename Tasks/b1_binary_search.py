5b0aa073a70154f7d50ea726f5f49bd998927c38
LEOSHA <leoshait@yandex.ru>
2019-12-19 20:20:47 +0300

    100 to 101

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -1,5 +1,5 @@
 from typing import Any, Sequence, Optional
-
+import random
 
 def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	"""
@@ -11,3 +11,30 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	"""
 	print(elem, arr)
 	return None
+
+def bn_search_elem(find_elem, array):
+	right = len(array)
+	left = 0
+	middle = (right + left)//2
+	while middle > -1:
+		print(f"{array[middle]}")
+		if find_elem == array[middle]:
+			return middle
+		else:
+			if array[middle] > find_elem:
+				right = middle
+				left = 0
+				middle = (right + left)//2
+			else:
+				right = len(array)
+				left = middle
+				middle = (right + left)//2
+
+
+
+if __name__ == "__main__":
+	array = [i for i in range(1, 11)]
+	find_elem = 5 #random.choice(array)
+	print(f"find_elem {find_elem}")
+	print(array)
+	print("index:", bn_search_elem(find_elem, array))
