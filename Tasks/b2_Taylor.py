bed763618138224115fc2473a20e8fc936a7e9b5
LEOSHA <leoshait@yandex.ru>
2019-12-19 20:52:49 +0300

    b2 exp done

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,9 +2,9 @@
 Taylor series
 """
 from typing import Union
+import math
 
-
-def ex(x: Union[int, float]) -> float:
+def ex(x: Union[int, float], delta=0.0001) -> float:
 	"""
 	Calculate value of e^x with Taylor series
 
@@ -12,15 +12,31 @@ def ex(x: Union[int, float]) -> float:
 	:return: e^x value
 	"""
 	print(x)
-	return 0
+	if x == 0:
+		return 1
+	else:
+		result_sum = 1
+		n = 1
+		while True:
+			result = (x ** n) / math.factorial(n)
+			if result < delta:
+				return result_sum
+			else:
+				result_sum += result
+				n += 1
 
 
-def sinx(x: Union[int, float]) -> float:
+
+def sinx(x: Union[int, float], delta=0.0001) -> float:
 	"""
 	Calculate sin(x) with Taylor series
 
 	:param x: x value
 	:return: sin(x) value
 	"""
+	result_sum = x
 	print(x)
 	return 0
+
+
+
