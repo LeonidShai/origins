5d14c9c230e886f9eb93fd3e66743b360343b9b2
leonid <leoshait@yandex.ru>
2019-12-24 00:32:34 +0300

    commit

--- Tasks/b1_binary_search.py
+++ Tasks/b1_binary_search.py
@@ -9,5 +9,12 @@ def binary_search(elem: Any, arr: Sequence) -> Optional[int]:
 	:param arr: array where element is to be found
 	:return: Index of element if it's presented in the arr, None otherwise
 	"""
+	arr = list(arr)
+	arr.sort()
+
 	print(elem, arr)
+
 	return None
+
+if __name__ == "__main__":
+	print(binary_search(3, [1, 6, 2, 3, 5, 9]))
