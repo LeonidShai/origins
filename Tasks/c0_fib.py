e676c94ecbd31531ca5281b9a7cc26d3de37c3e2
leonid <leoshait@yandex.ru>
2019-12-26 00:13:39 +0300

    c0_fib выполнено

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,7 +5,7 @@ def fib_recursive(n: int) -> int:
 	:param n: number of item
 	:return: Fibonacci number
 	"""
-	print(n)
+	#print(n)
 	if n < 0:
 		raise ValueError
 	elif n == 0:
@@ -23,11 +23,24 @@ def fib_iterative(n: int) -> int:
 	:param n: number of item
 	:return: Fibonacci number
 	"""
-	print(n)
+	#print(n)
+	a = 0
+	b = 1
 	if n < 0:
 		raise ValueError
-	return 0
+	elif n == 1:
+		return a
+	elif n == 2:
+		return b
+	else:
+		i = 2
+		chislo = 0
+		while i <= n:
+			chislo = a + b
+			a, b = b, chislo
+			i += 1
+		return chislo
 
 if __name__ == "__main__":
-	print(fib_recursive(3))
-	#print(fib_iterative(2))
+	print(fib_recursive(8))
+	print(fib_iterative(9))
