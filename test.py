3847106a7f72c73437cac3d520b3e219c0191ded
leonid <leoshait@yandex.ru>
2020-01-21 23:11:55 +0300

    comm

--- test.py
+++ test.py
@@ -32,7 +32,7 @@ def gl_find(gr, str, visited):
     #     return True
     visited[str] = True
     #print(visited)
-    #print(str, gr.adj[str])
+    print(str, gr.adj[str])
     # проход по графу
     for node in gr.adj[str]:
         if not visited[node]:  # если не посещали узел
@@ -89,6 +89,7 @@ if __name__ == "__main__":
         if not visited[node]:  # если не посещали узел
             gl_find(gr, node, visited)
             num += 1
+            print("-----")
     print(num)
 
     n = 10
